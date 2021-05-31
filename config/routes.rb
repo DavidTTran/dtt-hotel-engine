cd45ee1d7e3d8070afcc5a219788cced6029b0b9
David Tran <davidttemail@gmail.com>
2021-05-30 22:15:54 -0600

    Add CRUD methods

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       namespace :dnd do
         resources :classes, only: [:index]
         resources :monsters, only: [:index]
+        resources :parties
       end
     end
   end
