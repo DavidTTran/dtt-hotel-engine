36433eb1a9e33e32c0524a38717ace30878d3720
David Tran <davidttemail@gmail.com>
2021-05-30 22:15:27 -0600

    Update models

--- app/models/character.rb
+++ app/models/character.rb
@@ -16,6 +16,6 @@ class Character < ApplicationRecord
     "wizard"],
     message: "%{value} is not a valid class."
   }
-
-  validates :level, numericality: { only_integer: true, greater_than: 0, less_than: 20 }
+  validates :level, numericality: { only_integer: true, greater_than: 0, less_than: 20, message: "Level must be between 1 and 21." }
+  belongs_to :party, optional: true
 end
--- app/models/party.rb
+++ app/models/party.rb
@@ -1,3 +1,4 @@
 class Party < ApplicationRecord
+  validates_uniqueness_of :name, message: "A party by the name %{value} already exists."
   has_many :characters
 end
