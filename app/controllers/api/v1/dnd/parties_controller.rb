32c85620342671b0bf78c55b6e0f50957334cd76
David Tran <davidttemail@gmail.com>
2021-05-30 22:15:42 -0600

    Add CRUD methods

---
+++ app/controllers/api/v1/dnd/parties_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::Dnd::PartiesController < ApplicationController
+  def index
+    render json: Party.all
+  end
+
+  def create
+    party = Party.new(party_params)
+    if party.save
+      render json: Party.save
+    else
+      render json: { status: "error", code: 400, message: party.errors.messages }
+    end
+  end
+
+  def update
+    params = party_params
+    render json: Party.find(params[:id]).update(params)
+  end
+
+  def destroy
+    render json: Party.find(params[:id]).destroy
+  end
+
+  private
+
+  def party_params
+    params.permit(:name, :id)
+  end
+end
