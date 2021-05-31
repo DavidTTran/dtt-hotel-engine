dff1786cecb7e9ce64d2ac5e0dc71cc29a1fb47f
David Tran <davidttemail@gmail.com>
2021-05-30 20:07:56 -0600

    Update models

---
+++ app/models/character.rb
@@ -0,0 +1,21 @@
+class Character < ApplicationRecord
+  validates :name, presence: true
+  validates :character_class, inclusion: { in: [
+    "barbarian",
+    "bard",
+    "cleric",
+    "druid",
+    "fighter",
+    "monk",
+    "monster",
+    "paladin",
+    "ranger",
+    "rogue",
+    "sorcerer",
+    "warlock",
+    "wizard"],
+    message: "%{value} is not a valid class."
+  }
+
+  validates :level, numericality: { only_integer: true, greater_than: 0, less_than: 20 }
+end
